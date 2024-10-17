c4e8c7f5bfc3004f84348981f5b0173fd0aec321
Shanu-Ojha <ojhashanu2004@gmail.com>
2024-10-17 13:21:41 +0530

    loading page made

---
+++ app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',function(){
+    setTimeout(function(){
+        document.getElementById("lodingPage").style.display = 'none';
+        document.getElementById("mainPage").style.display = 'block';
+    },6500);
+});
