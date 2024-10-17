b39ac719536e1e66f396c973c34910bd9dc9728a
Shanu-Ojha <ojhashanu2004@gmail.com>
2024-10-17 18:44:04 +0530

    some bug fixed

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ window.addEventListener('load',function(){
     setTimeout(function(){
         document.getElementById("loadingPage").style.display = 'none';
         document.getElementById("mainPage").style.display = 'block';
-    },6000);
+    },7000);
 });
