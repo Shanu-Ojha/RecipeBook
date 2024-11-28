818016f51817a368c30338d9a89951249b897eae
Shanu-Ojha <ojhashanu2004@gmail.com>
2024-11-28 22:46:23 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
+    const navbar = document.getElementsByClassName('navBar')[0];
+    if (window.scrollY > 500) {
         navbar.style.backgroundColor = 'rgba(183, 95, 13, 0.8)';
     } else {
         navbar.style.backgroundColor = 'rgba(183, 95, 13, 0)';
