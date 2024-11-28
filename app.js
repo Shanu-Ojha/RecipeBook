c59445b516cd29bd0938a84664decb82c27477d6
Shanu-Ojha <ojhashanu2004@gmail.com>
2024-11-28 06:11:38 +0530

    updated

--- app.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load',function(){
-    setTimeout(function(){
-        document.getElementById("loadingPage").style.display = 'none';
-        document.getElementById("mainPage").style.display = 'block';
-    },7000);
-});
